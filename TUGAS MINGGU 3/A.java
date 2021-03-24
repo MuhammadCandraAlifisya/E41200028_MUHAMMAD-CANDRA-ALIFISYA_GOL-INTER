70001f5f64afeef420de9df5374f60f48d67bddb
MuhammadCandraAlifisya <75296155+MuhammadCandraAlifisya@users.noreply.github.com>
2021-03-24 17:28:44 +0700

    Create A.java

---
+++ TUGAS MINGGU 3/A.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author MUHAMMAD CANDRA
+ */
+public class TUGAS1 {
+    
+public static void main (String[] args){
+    String[] nama = {"MUHAMMAD", "CANDRA", "ALIFISYA"};
+    System.out.println("jumlah array = " + nama.length);
+    
+    
+    for (int i=0; i<nama.length; i++){
+        System.out.print(nama[i]);
+    }
+}
+}
+    
+
